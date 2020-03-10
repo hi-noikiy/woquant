cb3ed21baa50f6eb76d39e48897fa3429b991d88
liubin <liub67@chinaunicom.cn>
2020-03-11 01:33:44 +0800

    ..

--- module/analysis/估值报告.py
+++ module/analysis/估值报告.py
@@ -7,6 +7,7 @@ import pandas as pd
 from module.source.jqdata import JQData
 from config import PROJECT_DIR
 from utils.db import DBManager
+from utils.plot import plot_valuation_report
 
 # 配置文件
 jq_data = JQData()
@@ -16,3 +17,6 @@ stock_pool['close'] = stock_pool['code'].map(lambda x: jq_data.get_realtime_pric
 stock_pool['rate'] = stock_pool.apply(lambda x: round((x.close - x.price) / x.price * 100, 2), axis=1)
 stock_pool.sort_values(by='rate', ascending=False, inplace=True)
 db.write(stock_pool[['name', 'tag', 'price', 'close', 'rate']], 'valuation_report', mode='w')
+
+# 生成图片
+plot_valuation_report()

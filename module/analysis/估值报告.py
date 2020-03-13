15673d98dcf79f384c5f93f4d0482386f0933a94
liubin <liub67@chinaunicom.cn>
2020-03-13 18:34:24 +0800

    添加调度器

--- module/analysis/估值报告.py
+++ module/analysis/估值报告.py
@@ -4,20 +4,30 @@
 
 # 高抛低吸是赚不了大钱的，只有持有+复利，好股票+好价格，仓位管理 才能赚大钱！
 """
-import os
+import os, time
+import schedule
 import pandas as pd
 from module.source.jqdata import JQData
 from config import PROJECT_DIR
 from utils.db import DBManager
 from utils.plot import plot_all
 
-# 配置文件
-jq_data = JQData()
-db = DBManager()
-stock_pool = pd.read_csv(os.path.join(PROJECT_DIR, "config/pool.txt"), sep=' ', names=['tag', 'name', 'code', 'price'])
-stock_pool['close'] = stock_pool['code'].map(lambda x: jq_data.get_realtime_price(x))
-stock_pool['rate'] = stock_pool.apply(lambda x: round((x.close - x.price) / x.price * 100, 2), axis=1)
-stock_pool.sort_values(by='rate', ascending=False, inplace=True)
-db.write(stock_pool[['name', 'tag', 'price', 'close', 'rate']], 'valuation_report', mode='w')
 
-plot_all()
+# 配置文件
+def run():
+    jq_data = JQData()
+    db = DBManager()
+    stock_pool = pd.read_csv(os.path.join(PROJECT_DIR, "config/pool.txt"), sep=' ',
+                             names=['tag', 'name', 'code', 'price'])
+    stock_pool['close'] = stock_pool['code'].map(lambda x: jq_data.get_realtime_price(x))
+    stock_pool['rate'] = stock_pool.apply(lambda x: round((x.close - x.price) / x.price * 100, 2), axis=1)
+    stock_pool.sort_values(by='rate', ascending=False, inplace=True)
+    db.write(stock_pool[['name', 'tag', 'price', 'close', 'rate']], 'valuation_report', mode='w')
+
+    plot_all()
+
+
+schedule.every(10).minutes.do(run)
+while True:
+    schedule.run_pending()
+    time.sleep(1)

069d200b7c3e54cefa9da69eb9d55c605074bac4
liubin <liub67@chinaunicom.cn>
2020-04-19 21:24:55 +0800

    添加北上资金跟踪指标

--- module/analysis/北上资金.py
+++ module/analysis/北上资金.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from module.source.jqdata import JQData
 from jqdatasdk import finance
 from sqlalchemy.orm.query import Query
@@ -17,31 +17,36 @@ def run():
     jq = JQData()
     start_date = datetime.now().strftime("%Y%m%d")
     date_id = datetime.strftime(parse(start_date) + relativedelta(days=-30), '%Y%m%d')
-    stock_pool = pd.read_csv(os.path.join(PROJECT_DIR, "config/pool.txt"), dtype=str,sep=' ',
-                                 names=['tag', 'name', 'code', 'price'])
+    stock_pool = pd.read_csv(os.path.join(PROJECT_DIR, "config/pool.txt"), dtype=str, sep=' ',
+                             names=['tag', 'name', 'code', 'price'])
     stocks = stock_pool['code'].map(lambda x: jq.normalize_code(x)).tolist()
-    df=finance.run_query(Query(finance.STK_HK_HOLD_INFO).filter(
-        or_(finance.STK_HK_HOLD_INFO.link_id==i  for i in  [310001 , 310002] ),
-        or_(finance.STK_HK_HOLD_INFO.code==i  for i in stocks ),
-        finance.STK_HK_HOLD_INFO.day >=date_id
-    ))[['day','name','share_ratio']]
-    df['day'] = df['day'].map(lambda x : datetime.strftime(x,"%Y%m%d"))
+    df = finance.run_query(Query(finance.STK_HK_HOLD_INFO).filter(
+        or_(finance.STK_HK_HOLD_INFO.link_id == i for i in [310001, 310002]),
+        or_(finance.STK_HK_HOLD_INFO.code == i for i in stocks),
+        finance.STK_HK_HOLD_INFO.day >= date_id
+    ))[['day', 'name', 'share_ratio']]
+    df['day'] = df['day'].map(lambda x: datetime.strftime(x, "%Y%m%d"))
 
     trade_day = finance.run_query(Query(finance.STK_EXCHANGE_LINK_CALENDAR).filter(
-        finance.STK_EXCHANGE_LINK_CALENDAR.day>=date_id,
-        or_(finance.STK_EXCHANGE_LINK_CALENDAR.link_id==i  for i in [310001 , 310002] ))
-    )[['day','type']].drop_duplicates()
-    trade_day['day'] = trade_day['day'].map(lambda x : datetime.strftime(x,"%Y%m%d"))
+        finance.STK_EXCHANGE_LINK_CALENDAR.day >= date_id,
+        or_(finance.STK_EXCHANGE_LINK_CALENDAR.link_id == i for i in [310001, 310002]))
+    )[['day', 'type']].drop_duplicates()
+    trade_day['day'] = trade_day['day'].map(lambda x: datetime.strftime(x, "%Y%m%d"))
 
-    merge_df = df.merge(trade_day , on='day',how='left')
+    merge_df = df.merge(trade_day, on='day', how='left')
     merge_df = merge_df[merge_df['type'] == '正常交易日']
     del merge_df['type']
 
     db.write(merge_df, 'tail_northup', mode='w')
 
-# 配置每天20:10执行
-schedule.every().day.at("7:00").do(run)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == '1':
+        run()
+        exit(0)
+    # 配置每天20:10执行
+    schedule.every().day.at("07:00").do(run)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
